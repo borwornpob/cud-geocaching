ddfe01d454ecaa7c532c7de0b2be10236f7e1c5a
borwornpob <60835336+borwornpob@users.noreply.github.com>
2020-11-20 11:52:51 +0700

    include fonts in index.js and index.css

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
+import "./assets/fonts/ARCADE_N.TTF";
 
 ReactDOM.render(
   <React.StrictMode>
